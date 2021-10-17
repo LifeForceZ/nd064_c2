8836a97db37b1653824b311bcb0269193c0b3e75
elym <elym.biz@live.com>
2021-10-17 16:27:50 +0800

    update connection api

--- modules/location_api/app/udaconnect/controllers.py
+++ modules/location_api/app/udaconnect/controllers.py
@@ -15,6 +15,7 @@ api = Namespace("UdaConnect", description="Connections via geolocation.")  # noq
 
 
 # TODO: This needs better exception handling
+
 @api.route("/locations")
 @api.route("/locations/<location_id>")
 @api.param("location_id", "Unique ID for a given Location", _in="query")
@@ -22,9 +23,9 @@ class LocationResource(Resource):
     @accepts(schema=LocationSchema)
     @responds(schema=LocationSchema)
     def post(self) -> Location:
-        request.get_json()
-        location: Location = LocationService.create(request.get_json())
-        return location
+        payload = request.get_json()
+        new_location: Location = LocationService.create(payload)
+        return new_location
 
     @responds(schema=LocationSchema)
     def get(self, location_id) -> Location:
